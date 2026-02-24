c50daa1b95692f191e4a71ef4bcc07bf8468a579
Nidhi Mishra <mishra2004nidhi@gmail.com>
2026-02-24 22:15:59 +0530

    updated owner

--- init/index.js
+++ init/index.js
@@ -17,7 +17,7 @@ async function main() {
 
 const initDB= async()=>{
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) =>({...obj , owner: "68c3ed19d60bdaec5c693409"})); 
+    initData.data = initData.data.map((obj) =>({...obj , owner: "699da071b292b02a7b051651"})); 
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
